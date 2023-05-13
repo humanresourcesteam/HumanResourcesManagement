5377652467b470f3c0d1c82f556adbf3f37d014c
ozturkmelihcan <melihcannozturkk@gmail.com>
2023-05-13 08:42:59 +0300

    EMAIL HATALARINDAN DOLAYI METHODLARI KONTROL EDEMEDIM , CALISMAZLARSA IHTIYACINA GORE REVIZE EDERSIN...

---
+++ PermissionMicroService/src/main/java/com/bilgeadam/dto/response/WorkerPermissionForWorker.java
@@ -0,0 +1,33 @@
+package com.bilgeadam.dto.response;
+
+import com.bilgeadam.repository.enums.ApprovalStatus;
+import com.bilgeadam.repository.enums.TypeOfPermit;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class WorkerPermissionForWorker {
+
+
+
+    TypeOfPermit typeOfPermit;
+
+    LocalDate startDate;
+
+    LocalDate endDate;
+
+    LocalDate dateOfRequest;
+
+    Integer numberOfDays;
+
+    ApprovalStatus approvalStatus;
+
+    LocalDate replyDate;
+}
